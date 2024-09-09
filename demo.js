1aeba1f3ee2d688d3e63b466a7233f26abe4b238
KushalNerpagar <164306904+KushalNerpagar@users.noreply.github.com>
2024-09-09 12:33:48 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,6 @@
+// function setNeedle(angle) {
+//     document.querySelector('.needle').style.transform = `rotate(${angle}deg)`;
+// }
+
+// // Example: Set needle to 'Good' position (adjust angle as necessary)
+// setNeedle(90);
